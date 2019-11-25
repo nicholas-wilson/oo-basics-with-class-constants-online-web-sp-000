252ebd84ea0d8feb687726f9fdc5b3c9687dcdcf
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-25 20:31:45 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,27 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if (new_brand?(BRANDS, brand))
+      BRANDS << brand
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+
+  def new_brand?(brands, new_brand)
+    brands.each do |brand|
+      if brand == new_brand
+        return false
+      end
+    end
+    return true
   end
 
 end
