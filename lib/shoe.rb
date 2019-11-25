1c11a872633891aa5fd6266937988a81066b6c8d
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-25 20:12:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,14 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  @@counter = 1
+
   BRANDS = ["Uggs", "Rainbow"]
 
   def initialize(brand)
     @brand = brand
 
     BRANDS.each do |shoe|
-      puts "The #{@@counter} time running"
+
       if shoe == brand
         #don't add it to BRANDS since it's already there
         puts "The shoe brand was #{brand}"
@@ -17,7 +17,6 @@ class Shoe
         puts "Unique brand is : #{brand}"
       end
     end
-    @@counter += 1
   end
 
   def cobble
